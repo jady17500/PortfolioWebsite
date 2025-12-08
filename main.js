54cec872b208f7210b6b25b477e0017822276e5d
jady17500 <marco.piamonte04@gmail.com>
2025-12-08 18:09:14 +0100

    Init

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log(document)
+
+
+const git = document.getElementById("Git")
+git.addEventListener("click", () =>{
+    window.open("https://github.com/jady17500")
+})
+
+const linkedin = document.getElementById("Linkedin")
+linkedin.addEventListener("click", () =>{
+    window.open("https://www.linkedin.com/in/marco-piamonte-b78502247/")
+})
+
+const itch = document.getElementById("Itch")
+itch.addEventListener("click", () =>{
+    window.open("https://jady1750.itch.io/")
+})
